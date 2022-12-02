e50a73aa8707dba586fc1290502fc454f2546e50
Sarah <110748357+saraheita@users.noreply.github.com>
2022-12-02 21:38:05 +0200

    Add files via upload

---
+++ philosophers_problem/solution_using_semaphores/solution_using_semaphores/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace dining_philosophers
+{
+    class Dining_Philosophers
+    {
+        //Initializes semaphore object with two parameters (InitialCount, MaximumCount)
+        public static Semaphore semaphore = new Semaphore(2, 3);
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Thread threadObject = new Thread(eat)
+                {
+                    Name = "philosopher " + i
+                };
+                threadObject.Start();
+            }
+            Console.ReadKey();
+        }
+        static void eat()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " is thinking");
+            try
+            {
+                //Blocks the current thread until the current WaitHandle receives a signal.   
+                semaphore.WaitOne();
+                //Decrease the Initial Count Variable by 1
+                Console.WriteLine(Thread.CurrentThread.Name + " is eating");
+                Thread.Sleep(1000);
+                Console.WriteLine(Thread.CurrentThread.Name + " stopped eating");
+            }
+            finally
+            {
+                //Release() method to release semaphore  
+                //Increase the Initial Count Variable by 1
+                semaphore.Release();
+            }
+        }
+    }
+}
