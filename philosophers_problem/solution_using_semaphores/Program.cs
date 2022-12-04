05b4b38a023f85419b73f70b703aafe712d7060c
Sarah <110748357+saraheita@users.noreply.github.com>
2022-12-04 18:39:17 +0200

    Add files via upload

---
+++ philosophers_problem/solution_using_semaphores/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Xml.Linq;
+
+namespace Dining_Philosophers
+{
+    class Dining_Philosophers
+    {
+        //semaphore class lets you set a limit on the number of threads that have access to a critical section
+        //semaphore class is used to control access to a pool of resources
+
+        //Initializes semaphore object with two parameters (InitialCount, MaximumCount)
+        public static Semaphore semaphore = new Semaphore(2, 3);
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                //creates and initializes the thread
+                Thread threadObject = new Thread(eat)
+                {
+                    Name = "philosopher " + i
+                };
+                //calls thread to start process
+                threadObject.Start();
+            }
+            Console.ReadKey();
+        }
+        static void eat()
+        {
+            //Fetches the name of the current thread using Name property
+            Console.WriteLine(Thread.CurrentThread.Name + " is thinking");
+            try
+            {
+                //Blocks the current thread until the current WaitHandle receives a signal.   
+                semaphore.WaitOne();
+                //Decrease the Initial Count Variable by 1
+                Console.WriteLine(Thread.CurrentThread.Name + " is eating");
+                //Thread.Sleep() temporarily suspends the current execution of the thread for specified millisecond
+                //so that other threads can get the chance to start the execution
+                Thread.Sleep(100);
+                Console.WriteLine(Thread.CurrentThread.Name + " stopped eating");
+            }
+            finally
+            {
+                //Release() method to release semaphore  
+                //Increase the Initial Count Variable by 1
+                semaphore.Release();
+            }
+        }
+    }
+}
---
+++ philosophers_problem/solution_using_semaphores2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace Dining_Philosphers2
+{
+    class Dining_Philosphers2
+    {
+        static Semaphore[] Forks = new Semaphore[5];
+        static void Main(string[] args)
+        {
+            //Forks.Length returns the total number of elements in th array
+            for (int i = 0; i < Forks.Length; i++)
+            {
+                Forks[i] = new Semaphore(1, 1);
+            }
+            //Creating and initializing threads
+            Thread ph1 = new Thread(() => { set(0); });
+            Thread ph2 = new Thread(() => { set(1); });
+            Thread ph3 = new Thread(() => { set(2); });
+            Thread ph4 = new Thread(() => { set(3); });
+            //ThreadState.Running
+            ph1.Start();
+            ph2.Start();
+            ph3.Start();
+            ph4.Start();
+        }
+        static void set(int ph_num)
+        {
+            do
+            {
+                Forks[ph_num].WaitOne();
+                Forks[(ph_num + 1) % 4].WaitOne();
+                Console.WriteLine("philosopher{0} is eating", ph_num);
+                Forks[ph_num].Release();
+                //Exits the semaphore
+                Forks[(ph_num + 1) % 4].Release();
+                Console.WriteLine("philosopher{0} is thinking", ph_num);
+                //Suspends the currect thread
+                Thread.Sleep(100);
+            } while (true);
+        }
+
+    }
+}
